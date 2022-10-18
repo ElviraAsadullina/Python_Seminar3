94a76e6c5c577080f60eecd9ecf1150181424fcb
Elvira <eliasufa@yahoo.com>
2022-10-18 14:43:29 +0500

    Задача 5 исправлена: установлен вывод для N чисел

--- Task_5.py
+++ Task_5.py
@@ -11,22 +11,24 @@ def check_for_input_error():
         break
     return a
 def fibonacci(n):
-    if (n <= 1):
-        return n
+    if n == 0:
+        return [0]
+    elif n == 1:
+        return [0, 1]
     else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
-def full_fibonacci(n):
-    positive_fib = []
+        lst = fibonacci(n-1)
+        lst.append(lst[-1] + lst[-2])
+        return lst
+def full_fibonacci(n, fib_list):
     negative_fib = []
-    for i in range(number):
-        positive_fib.append(fibonacci(i))
-    for i in range(len(positive_fib)):
+    for i in range(len(fib_list)):
         if i % 2 == 0 and i != 0:
-            negative_fib.append(positive_fib[i] * -1)
+            negative_fib.append(fib_list[i] * -1)
         if i % 2 != 0:
-            negative_fib.append(positive_fib[i])
+            negative_fib.append(fib_list[i])
     negative_fib.reverse()
-    full_fibonacci = negative_fib + positive_fib
+    full_fibonacci = negative_fib + fib_list
     return full_fibonacci
 number = check_for_input_error()
-print(f'Последовательность Фибоначчи (включая негафибоначчи) для данного числа: {full_fibonacci(number)}')
+positive_fib = fibonacci(number)
+print(f'Последовательность Фибоначчи (включая негафибоначчи) для данного числа: {full_fibonacci(number, positive_fib)}')
